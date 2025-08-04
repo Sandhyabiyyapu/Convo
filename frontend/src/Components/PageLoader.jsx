affd6c1ef6ac4c7acc67b5614aa61aeacb87423c
Sandhyabiyyapu <sandhyabiyyapu5@gmail.com>
2025-08-04 19:32:47 +0530

    Home page added

--- frontend/src/Components/PageLoader.jsx
+++ frontend/src/Components/PageLoader.jsx
@@ -1,8 +1,12 @@
-const PageLoader = () =>{
-    return(
-        <div className="min-h-screen flex items-center justify-center">
-            <LoaderIcon className="animate-spin size-10 text-primary"/>
+import { LoaderIcon } from "lucide-react";
+import { useThemeStore } from "../Store/useThemeStore";
 
-        </div>
-    )
-}
+const PageLoader = () => {
+  const { theme } = useThemeStore();
+  return (
+    <div className="min-h-screen flex items-center justify-center" data-theme={theme}>
+      <LoaderIcon className="animate-spin size-10 text-primary" />
+    </div>
+  );
+};
+export default PageLoader;
